8158a2f9d6401ddfca15dd6eb818518cd12f5b6d
Apander <Apander204@gmail.com>
2025-11-28 21:19:19 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const path = require('path');
+const express = require('express');
+require('dotenv').config();
+const app = express();
+
+app.use(express.static('public'));
+
+const PORT = process.env.PORT;
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'files', 'index.html'));
+});
+app.get("/about", (req, res) => {
+    res.sendFile(path.join(__dirname, 'files', 'about.html'));
+});
+app.get("/contact-me", (req, res) => {
+    res.sendFile(path.join(__dirname, 'files', 'contact-me.html'));
+});
+app.use((req, res) => {
+    res.status(404);
+    res.sendFile(path.join(__dirname, 'files', '404.html'));
+});
+
+app.listen(PORT || 3000, (req, res) => {
+    console.log("Port listen on port", PORT || 3000);
+});
